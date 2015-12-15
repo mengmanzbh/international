907f0f3072c4798b4358e9a3fe86903cc25f132b
mengmanzbh <1934068305@qq.com>
2015-12-15 14:46:26 +0800

    zbh
    iOS 应用名称国际化  文字国际化 图片国际化  storyboard国际化

--- international/ViewController.m
+++ international/ViewController.m
@@ -5,7 +5,8 @@
 //  Created by kuailefu on 15/12/15.
 //  Copyright © 2015年 kuailefu. All rights reserved.
 //
-
+//获取本地化语言文件
+#define L(name) NSLocalizedString(name, nil)
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -17,6 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UILabel *lab = [[UILabel alloc]initWithFrame:CGRectMake(100, 100, 100, 35)];
+    lab.text = L(@"Take photo");
+    [self.view addSubview:lab];
+    
+    //获得所支持的语言
+    NSLog(@"%@",[[NSUserDefaults standardUserDefaults]objectForKey:@"AppleLanguages"]);
+    //获取当前使用语言
+    NSArray *languages = [NSLocale preferredLanguages];
+    NSString *currentLanguage = [languages objectAtIndex:0];
+    NSLog ( @"%@" , currentLanguage);
 }
 
 - (void)didReceiveMemoryWarning {
